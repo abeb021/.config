c5cfc92fc6a2a487ef4dc09819dfa8cbd21c5a9c
abeb021 <albertars021@gmail.com>
2026-06-30 21:58:41 +0300

    delete eww

--- eww/scripts/get_kb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Simple approach to get keyboard layout
-hyprctl devices | grep -A 20 "at-translated-set-2-keyboard" | grep "active_keymap" | awk '{print $2}' | head -c 2 | tr '[:lower:]' '[:upper:]'
